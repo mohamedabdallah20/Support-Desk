34bc1646e5e6dbd29fa49ea6a3589cbd441ee2f5
mohamedabdallah20 <mohamedabdala1442000@gmail.com>
2023-10-04 18:17:31 +0300

    Add login functionality and Setup JWT functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require('express-async-handler')
 const bcrypt = require('bcryptjs')
 const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
 
 // @desc    Register a new user
 // @route   POST /api/users
@@ -39,18 +40,42 @@ const registerUser = asyncHandler(async (req, res) => {
       _id: user._id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id),
     })
   } else {
     res.status(400)
     throw new Error('Invalid user data')
   }
 })
+
 // @desc    Login a user
 // @route   POST /api/users/login
 // @access  Public
 const loginUser = asyncHandler(async (req, res) => {
-  res.send('Login Route')
+  const { email, password } = req.body
+
+  const user = await User.findOne({
+    email,
+  })
+  // check user and password match
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(200).send({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    })
+  } else {
+    res.status(401)
+    throw new Error('Invalid Credentials')
+  }
 })
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  })
+}
 
 module.exports = {
   registerUser,
