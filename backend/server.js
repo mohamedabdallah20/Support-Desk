2defa37bca80785d9a3ab60157d0a107052df079
mohamedabdallah20 <mohamedabdala1442000@gmail.com>
2023-10-04 16:33:47 +0300

    Basic express server setup

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 const express = require('express')
+const dotenv = require('dotenv').config()
 
-const PORT = process.env.PORT | 8888
+const PORT = process.env.PORT || 8888
 
 const app = express()
+
+app.get('/', (req, res) => {
+  res.json({ message: 'GET request to the homepage' })
+})
 
 app.listen(PORT, () => {
   console.log(`SERVER RUNNING ON PORT ${PORT}`)
