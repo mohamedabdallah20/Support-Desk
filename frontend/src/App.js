19fe9353ae38264b741e198c7a4b7de390a3a3c6
mohamedabdallah20 <mohamedabdala1442000@gmail.com>
2023-10-05 02:53:21 +0300

    Initial Front-End commit with some basic components

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Register from './pages/Register'
+import Login from './pages/Login'
+import Header from './components/Header'
+function App() {
+  return (
+    <Router>
+      <div className="container">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
+  )
+}
+
+export default App
