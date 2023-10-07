d559e679e4e3b7c2a5e0963a1bd9620807cf95bb
mohamedabdallah20 <mohamedabdala1442000@gmail.com>
2023-10-07 16:28:10 +0300

    Setup getTickets and createTicket

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -7,14 +7,35 @@ const Ticket = require('../models/ticketModel')
 // @route   GET /api/tickets
 // @access  Private
 const getTickets = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'get tickets' })
+  //   get user
+  const user = req.user
+
+  if (!user) {
+    res.status(401)
+    throw new Error('User Not found')
+  }
+  const tickets = await Ticket.find({
+    user: user._id,
+  })
+
+  res.status(200).json({ tickets })
 })
 
 // @desc    Create new ticket
 // @route   POST /api/tickets
 // @access  Private
 const createTicket = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'create ticket' })
+  const { product, description } = req.body
+  if (!product || !description) {
+    res.status(400)
+    throw new Error('Please Add product and description')
+  }
+  const ticket = await Ticket.create({
+    product,
+    description,
+    user: req.user._id,
+  })
+  res.status(201).json({ ticket })
 })
 
 module.exports = {
